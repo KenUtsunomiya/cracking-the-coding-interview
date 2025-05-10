eb907f59d76d5474270d30a3f622bd011d3e0eab
Ken Utsunomiya <kenutsunomiya08@gmail.com>
2025-05-10 18:06:49 +0900

    fix Solution template

--- template/Solution.java
+++ template/Solution.java
@@ -1,5 +1,18 @@
-public class Solution {
+class Solution {
+
     public void solve() {
         System.out.println("Hello, World!");
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // TODO: Add test cases with assert
+
+        System.out.println("--------------------");
+        System.out.println("All test cases passed!");
+        System.out.println("--------------------");
     }
 }
